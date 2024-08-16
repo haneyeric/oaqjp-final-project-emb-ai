99b1fa459d230b101b86b050676482ac9ff2ea3d
haneyeric <haneyeric@gmail.com>
2024-08-16 11:02:23 -0400

    update output format

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -6,5 +7,16 @@ def emotion_detector(text_to_analyze):
     input_obj = { "raw_document": { "text": text_to_analyze } }
 
     res = requests.post(url, json = input_obj, headers = head)
+    res_dict = json.loads(res.text)
+    emotion_dict = res_dict["emotionPredictions"][0]["emotion"]
+    dominant_emotion = ""
+    max_emotion = 0
 
-    return res.text
+    for key in emotion_dict:
+        if emotion_dict[key] > max_emotion:
+            max_emotion = emotion_dict[key]
+            dominant_emotion = key
+    
+    emotion_dict['dominant_emotion'] = dominant_emotion
+
+    return emotion_dict
